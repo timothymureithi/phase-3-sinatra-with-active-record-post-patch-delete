530c3adc75f0103d6b4db6266b201b72ccb10d5b
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-03 02:59:24 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < Sinatra::Base
     games = Game.all.order(:title).limit(10)
     games.to_json
   end
-
   get '/games/:id' do
     game = Game.find(params[:id])
 
@@ -15,5 +14,45 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
+
+  get '/reviews' do
+    review = Review.all.order(:title).limit(10)
+    review.to_json
+  end
+
+  # get '/reviews:id' do
+  #   review = Review.find(params[:id])
+  #   review.to_json
+  # end
+
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
 
 end
